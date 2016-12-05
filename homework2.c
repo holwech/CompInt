2246e5440260b7bd6ed38a6d9ce4638d9f40a2f9
Holwech <ga48bum@linux68.clients.eikon.tum.de>
2016-12-05 12:47:29 +0100

    Work on ubung 2

--- homework2.c
+++ homework2.c
@@ -3,16 +3,34 @@
 
 #define CAPACITY 1000
 
-typedef struct {
+void assert(int expr) {
+  if (expr) {
+    printf(" ...OK\n");
+  } else {
+      printf(" ...FAILED\n");
+  }
+}
+
+  typedef struct Vector {
     int size;
     int cap;
-    void* data;
+    void** data;
 } Vector;
 
 void initVector(Vector* vector, int initSize) {
    vector->size = 0;
    vector->cap = initSize;
    vector->data = (void *)malloc(initSize * sizeof(void));
+}
+
+void test_initVector() {
+  printf("%s\n", " ----- Test initVector -----");
+  struct Vector* vector;
+  initVector(vector, 100);
+  printf("%s\n", "Size");
+  assert(vector->size == 0);
+  printf("%s\n", "Capacity");
+  assert(vector->cap == 100);
 }
 
 void VectorPush(Vector* vector, void* data) {
@@ -25,8 +43,88 @@ void VectorPush(Vector* vector, void* data) {
   vector->size++;
 }
 
-// ----- END VECTOR -----
+void test_vectorPush() {
+  printf("%s\n", "----- Test vectorPush -----");
+  struct Vector* vector1;
+  initVector(vector1, 10);
+  vectorPush(vector1, 22);
+  printf("%s\n", "Push int");
+  assert(vector1->data[0] == 22);
+  printf("%s\n", "Push int size");
+  assert(vector1->size == 1);
+  printf("%s\n", "Push int cap");
+  assert(vector1->size == 10);
+  int i;
+  for (i = 0; i <= 10; i++) {
+    vectorPush(vector1, i + 3);
+  }
+  printf("%s\n", "Overflow");
+  assert(vector1->size == 10);
+}
 
+// Set all values in the vector to a spesific value
+// If random is true, all values will be set to a low random value
+void setAllValues(Vector* vector, int value, int random) {
+  if (random == 0) {
+      int i;
+      for (i = 0; i < vector->size; i++) {
+        vector[i] = value;
+      }
+  } else {
+    srand((unsigned int) time(NULL));
+    int i;
+    for (i = 0; i < vector.size; i++) {
+      double r = ((rand() % 100) + 1) / 10000;
+      vector->data[i] = r;
+    }
+
+  }
+}
+
+void test_setAllValues() {
+  struct Vector* vector;
+  initVector(vector, 10);
+  setAllValues(vector, 22);
+  int allSet = 1;
+  int i;
+  for (i = 0; i < vector; i++) {
+    if (vector->data[i] != 22) {
+      allSet = 0;
+    }
+  }
+  printf("%s\n", "Set all values");
+  assert(allSet == 0);
+}
+
+void printVector(Vector* vector, char type) {
+  printf("%s\n", "----- Vector -----");
+  printf("Size: %d\n", vector->size);
+  printf("Cap: %d\n", vector->);
+  printf("Data: ");
+  int i;
+  for (i = 0; i < vector->size; i++) {
+    print(vector, type);
+  }
+}
+
+void print(Vector* vector, char type) {
+  if (type == 'i') {
+    int i;
+    for (i = 0; i < vector->size; i++) {
+      printf("%d ", vector->data[i]);
+    }
+    printf("\n");
+  } else if (type == 'c') {
+    int i;
+    for (i = 0; i < vector->size; i++) {
+      printf("%c ", vector->data[i]);
+    }
+    printf("\n");
+  }
+}
+
+// ----- END VECTOR -----
+/*
 typedef struct {
     int size;
     int cap;
@@ -56,18 +154,40 @@ void init2DVector(Vector* vector, int size1, int size2) {
     }
 }
 
-
+*/
 // ----- END 2DVECTOR
-
+/*
 typedef struct {
-    Vector biases;
-
-    Vector sizes;
+    Vector* biases;
+    Vector* weights;
+    Vector* sizes;
     int layers;
 } Network;
 
 void initNetwork(Network* network, Vector* sizes) {
-    srand((unsigned int) time(NULL));
-    initVector(&network.biases, sizes.size - 1);
+    struct Vector* weights;
+    int rows = sizes->size - 1;
+    initVector(weights, rows);
+    int row;
+    for (row = 0; row < rows; row++) {
+      struct Vector*
+    }
 
+
+    network.weights = weights;
+
+    struct Vector* biases;
+    setAllValues(biases, 0, 1);
+    initVector(biases, sizes->size - 1);
+
+    network.data = biases;
+
+}
+*/
+
+int main() {
+  test_initVector();
+  test_vectorPush();
+  test_setAllValues();
+  return (0);
 }
