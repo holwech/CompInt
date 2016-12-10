ee2d1425b213779d39eaabbdff0aee072bfd55c0
Joachim Holwech <joachim.holwech@gmail.com>
2016-12-10 15:31:06 +0100

    changes

--- homework2_v2.c
+++ homework2_v2.c
@@ -1,10 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
 typedef struct {
     double data[1000];
-    size;
+    int size;
 } DVector;
 
+void initDVector(DVector* vector) {
+  vector->size = 0;
+}
+
 void DVPush(DVector* vector, double val) {
-    vector->data[size] = val;
+    vector->data[vector->size] = val;
     vector->size++;
 }
 
@@ -15,47 +22,36 @@ void DVSetAll(DVector* vector, double val, int end) {
     }
 }
 
-void DVSetAllRandom(DVector* vector, int end) {
-    int i;
-    for (i = 0; i < end; i++) {
-        DVPush(vector, randomVal(100, 50, 1000));
-    }
-}
-
 double randomVal(int max, int offset, int divisor) {
-  srand(time(NULL));
+  srand(time(0));
   return (rand() % max - offset) / divisor;
 }
 
+void DVSetAllRandom(DVector* vector, int end) {
+    int i;
+    for (i = 0; i < end; i++) {
+        double r = randomVal(100, 50, 1000);
+        DVPush(vector, r);
+    }
+}
+
+
+// END DVECTOR
+
+typedef struct {
+  int data[1000];
+    int size;
+} IVector;
+
+void IVPush(IVector* vector, int val) {
+    vector->data[vector->size] = val;
+    vector->size++;
+}
+
+// END IVECTOR
 typedef struct {
   DVector weights;
 } Perceptron;
-
-
-int feedForward(Perceptron* perc, Vector* input) {
-  float total = 0;
-  int i;
-  for(i = 0; i < input.size; i++) {
-    total += sigmoid()
-    total += *((double*)input->data[i]) * *((double*)perc->weights->data[i]);
-  }
-  if (total > 0) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-
-void train(Vector* perc, Vector* inputs, int answer) {
-  double LR = 0.01;
-  int ff = feedForward(perc, inputs);
-  float error = answer - ff;
-  int i;
-  for(i = 0; i < perc->weights->size; i++) {
-    *((double*)perc->weights->data[0]) = LR * error * *((int*)inputs->data[i]);
-  }
-}
 
 double sigmoid(double z) {
     return 1.0 / (1.0 + exp(-z));
@@ -65,10 +61,40 @@ double feedForward(Perceptron* perc, DVector* input) {
     double total = 0;
     int i;
     for (i = 0; i < input->size; i++) {
-        total += input->data[i] * perc->weights->data[i];
+        total += input->data[i] * perc->weights.data[i];
     }
-    total = sigmoid(total)
+    total = sigmoid(total);
     return activate(total);
+}
+
+void train(Perceptron* perc, DVector* inputs, int answer) {
+  double LR = 0.01;
+  double ff = feedForward(perc, inputs);
+  float error = answer - ff;
+  if (1) {
+    printf("Input: %lf, %lf, %lf\n", inputs->data[0], inputs->data[1], inputs->data[2]);
+    int j;
+    for (j = 0; j < inputs->)
+    printf("Before weights: ");
+    int p;
+    for (p = 0; p < perc->weights.size; p++) {
+      printf("%lf, ", perc->weights[p]);
+    }
+    printf("\n");
+    printf("Answer: %d ff: %lf Error: %lf\n", answer, ff, error);
+  }
+  int i;
+  for(i = 0; i < perc->weights.size; i++) {
+    perc->weights.data[i] = LR * error * inputs->data[i];
+  }
+  if (1) {
+    printf("After weights: ");
+    int p;
+    for (p = 0; p < perc->weights.size; p++) {
+      printf("%lf ", perc->weights[p]);
+    }
+    printf("\n");
+  }
 }
 
 int activate(double val) {
@@ -79,17 +105,38 @@ int activate(double val) {
     }
 }
 
-
 void runPerceptron() {
     Perceptron perc;
+    initDVector(&perc.weights);
     DVSetAllRandom(&perc.weights, 3);
 }
 
 // END PERCEPTRON
 
+void readData(DVector* input) {
+  int stop = 0;
+  char str[100];
+  int i;
+  while(stop == 0)  {
+    double x, y, a;
+    char ch;
+    scanf("%lf%c%lf%c%lf", &x, &ch, &y, &ch, &a);
+    if(0) {
+      printf("%lf %lf %lf\n", x, y, a);
+    }
+    DVPush(input, x);
+    DVPush(input, y);
+    DVPush(input, a);
+    if (x == 0.0 && y == 0.0 && a == 0.0) {
+      stop = 1;
+    }
+  }
+}
+
 int main() {
+  DVector input;
+  initDVector(&input);
+  readData(&input);
 
-
-  runTests();
   return (0);
 }
