0e416109243b7d68b539f3ff4053ba0c48beabe1
Joachim Holwech <joachim.holwech@gmail.com>
2017-01-24 12:31:27 +0100

    Changes

--- EX4/hw4_q1.c
+++ EX4/hw4_q1.c
@@ -11,8 +11,8 @@ typedef enum { false, true } bool;
 #define MAX_POINTS 1000
 #define CALC_N 2
 
-double alpha = 0.3;
-double sigma = 1.0;
+double k_de[4] = {0.25, 0.25, 0.5, 0.5};
+double k_te[4] = {0.12, 0.25, 0.12, 0.25};
 
 // END ARRAY
 
@@ -174,7 +174,7 @@ void CVCopy(CVector* vector, CVector copiedFrom) {
 typedef struct {
   double x;
   double y;
-  double delta;
+  double theta;
 } Point;
 
 double euclidean(Point p1, Point p2) {
@@ -228,8 +228,11 @@ void PVPrint(PVector* vector) {
 // END POINT ARR
 
 typedef struct {
-  PVector input1;
-  PVector input2;
+  PVector input_r;
+  PVector input_c;
+  double u[2][4];
+  double w[4];
+  double output[2];
 } Fuzzy;
 
 // END FUZZY
@@ -266,18 +269,69 @@ double calc_u_theta_large(double theta) {
   return 1 - calc_u_theta_small(theta);
 }
 
+double aggr_min(double u_de, double u_te) {
+  return min(u_de, u_te);
+}
+
+double aggr_prod(double u_de, double u_te) {
+  return u_de * u_te;
+}
+
+void calc_u(Fuzzy* fl, double d_e, double theta_e) {
+  for (int i = 0; i < 4; i++) {
+    double v_l = k_de[i] * d_e + k_te[i] * theta_e;
+    double v_r = k_de[i] * d_e - k_te[i] * theta_e;
+    fl->u[0][i] = v_l;
+    fl->u[1][i] = v_r;
+  }
+}
+
+void calc_w(Fuzzy* fl, double u_de_small, double u_de_large, double u_theta_small, double u_theta_large) {
+  fl->w[0] = aggr_prod(u_de_small, u_theta_small);
+  fl->w[1] = aggr_prod(u_de_small, u_theta_large);
+  fl->w[2] = aggr_prod(u_de_large, u_theta_small);
+  fl->w[3] = aggr_prod(u_de_large, u_theta_large);
+}
+
+void calc_output(Fuzzy* fl) {
+  double v_l_n = 0, v_l_d = 0, v_r_n = 0, v_r_d = 0;
+  for (int i = 0; i < 4; i++) {
+    v_l_n += fl->w[i] * fl->u[0][i];
+    v_l_d += fl->w[i];
+    v_r_n += fl->w[i] * fl->u[1][i];
+    v_r_d += fl->w[i];
+  }
+  fl->output[0] = v_l_n / v_l_d;
+  fl->output[1] = v_r_n / v_r_n;
+}
+
+void readFromFile(Fuzzy*);
+void readFromConsole(Fuzzy* fl);
 void run() {
   Fuzzy fl;
-  initPVector(&fl.input1);
-  initPVector(&fl.input2);
-  for (int i = 0; i < fl.size; i++) {
-    double d_e = euclidean(fl.input1.d[i], fl.input2.d[i]);
-    theta_e = theta_r - theta_c;
-    u_de_small = calc_u_theta_small(d_e);
-    u_de_large = calc_u_theta_large(d_e);
-    u_theta_small = calc_u_theta_small(theta_e);
-    u_theta_large = calc_u_theta_large(theta_e);
-    printf("%lf,%lf,%lf,%lf\n", u_de_small, u_de_large, u_theta_small, u_theta_large);
+  initPVector(&fl.input_r);
+  initPVector(&fl.input_c);
+  //readFromFile(&fl);
+  readFromFile(&fl);
+  for (int i = 0; i < fl.input_r.size; i++) {
+    double d_e = euclidean(fl.input_r.d[i], fl.input_c.d[i]);
+    double theta_e = fl.input_r.d[i].theta - fl.input_c.d[i].theta;
+    double u_de_small = calc_u_de_small(d_e);
+    double u_de_large = calc_u_de_large(d_e);
+    double u_theta_small = calc_u_theta_small(theta_e);
+    double u_theta_large = calc_u_theta_large(theta_e);
+    calc_u(&fl, d_e, theta_e);
+    calc_w(&fl, u_de_small, u_de_large, u_theta_small, u_theta_large);
+    calc_output(&fl);
+    if (DEBUG) {
+      printf("--------------\n");
+      printf("%lf,%lf,%lf,%lf\n", u_de_small, u_de_large, u_theta_small, u_theta_large);
+      printf("d_e: %lf, theta_e: %lf\n", d_e, theta_e);
+      printf("v_l u1: %lf, u2: %lf, u3: %lf, u4: %lf\n", fl.u[0][0], fl.u[0][1], fl.u[0][2], fl.u[0][3]);
+      printf("v_r u1: %lf, u2: %lf, u3: %lf, u4: %lf\n", fl.u[1][0], fl.u[1][1], fl.u[1][2], fl.u[1][3]);
+      printf("w1: %lf, w2: %lf, w3: %lf, w4: %lf\n", fl.w[0], fl.w[1], fl.w[2], fl.w[3]);
+    }
+    printf("%lf,%lf\n", fl.output[0], fl.output[1]);
   }
 }
 
@@ -292,12 +346,18 @@ void readFromFile(Fuzzy* fl) {
     printf("File open failed\n");
   }
   while ((read = getline(&line, &length, filePtr)) != -1) {
-      int xr, yr, tr, xc, yc, tc;
-      sscanf(line,"%lf,%lf,%lf,%lf,%lf,%lf", &xr, &yr, %tr, &xc, &yc, &tc);
+      double xr, yr, tr, xc, yc, tc;
+      sscanf(line,"%lf,%lf,%lf,%lf,%lf,%lf", &xr, &yr, &tr, &xc, &yc, &tc);
       Point pr = {xr, yr, tr};
       Point pc = {xc, yc, tc};
-      PVPush(&fl->input1, pr);
-      PVPush(&fl->input2, pc);
+      PVPush(&fl->input_r, pr);
+      PVPush(&fl->input_c, pc);
+  }
+  if (DEBUG) {
+    for (int i = 0; i < fl->input_r.size; i++) {
+      printf("xr: %lf, yr: %lf, tr: %lf, xc: %lf, yc: %lf, tc: %lf\n", fl->input_r.d[i].x, fl->input_r.d[i].y, fl->input_r.d[i].theta, fl->input_c.d[i].x, fl->input_c.d[i].y, fl->input_c.d[i].theta);
+    }
+    printf("End read\n");
   }
   fclose(filePtr);
   if (line) {
@@ -305,9 +365,28 @@ void readFromFile(Fuzzy* fl) {
   }
 }
 
+void readFromConsole(Fuzzy* fl) {
+  char line[1000];
+  while (scanf("%s", line) != EOF) {
+      double xr, yr, tr, xc, yc, tc;
+      sscanf(line,"%lf,%lf,%lf,%lf,%lf,%lf", &xr, &yr, &tr, &xc, &yc, &tc);
+      Point pr = {xr, yr, tr};
+      Point pc = {xc, yc, tc};
+      PVPush(&fl->input_r, pr);
+      PVPush(&fl->input_c, pc);
+  }
+  if (DEBUG) {
+    for (int i = 0; i < fl->input_r.size; i++) {
+      printf("xr: %lf, yr: %lf, tr: %lf, xc: %lf, yc: %lf, tc: %lf\n", fl->input_r.d[i].x, fl->input_r.d[i].y, fl->input_r.d[i].theta, fl->input_c.d[i].x, fl->input_c.d[i].y, fl->input_c.d[i].theta);
+    }
+    printf("End read\n");
+  }
+}
+
 // END SOM
 
 int main() {
   srand(time(0));
+  run();
 	return (0);
 }
